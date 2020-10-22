35e7c8386b1974a524274879d3f57ecb723e1f2d
JFelixZuniga <johnfzuniga@gmail.com>
2020-10-21 21:33:49 -0300

    first commit

---
+++ static/main.js
@@ -0,0 +1,27 @@
+async function getPokemon(id) {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+  const data = await response.json();
+  return data;
+}
+
+function init() {
+  const firstPokemon = await getPokemon(150)
+  // console.log(getPokemon(25)   )
+}
+
+init();
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector(".floating-btn");
+const close_btn = document.querySelector(".close-btn");
+const social_panel_container = document.querySelector(
+  ".social-panel-container"
+);
+
+floating_btn.addEventListener("click", () => {
+  social_panel_container.classList.toggle("visible");
+});
+
+close_btn.addEventListener("click", () => {
+  social_panel_container.classList.remove("visible");
+});
